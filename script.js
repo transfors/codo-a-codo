a6b9d485a3d43a6b27d9333263e44347c71faf2c
transfors <transfor2008@gmail.com>
2023-05-05 10:45:16 -0300

    arreglos menu carousel footer

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function calcularPrecioTotal() {
     let cantEnt = document.getElementById("CantidadEntradas").value;
     document.getElementById("titulo").innerHTML =
-    "Comprar " + cantEnt + " entradas";
+        "Comprar " + cantEnt + " entradas";
     let porcDesc = document.getElementById("porcDescuento").value;
     let descuento = (cantEnt * 1000 * porcDesc) / 100;
     let precioFin = cantEnt * 1000 - descuento;
     document.getElementById("precioFinal").value = precioFin;
 }
+
